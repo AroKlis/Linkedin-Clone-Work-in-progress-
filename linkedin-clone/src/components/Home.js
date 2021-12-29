eee22fde5f18e7eb366a45ce835a15a09623686d
Karolis Kubilius <41202263+AroKlis@users.noreply.github.com>
2021-12-29 06:00:09 +0000

    Created Header component

---
+++ linkedin-clone/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from './Header';
+
+function Home() {
+    return (
+        <Header />
+    )
+}
+
+export default Home
