793647b8c1ca8012089fa3ab14996f8c98b6776f
Karolis Kubilius <41202263+AroKlis@users.noreply.github.com>
2022-01-08 05:49:04 +0000

    Completed Front-End side of "/home" page

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,16 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Login from './components/Login';
-import Header from './components/Header';
-import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path='/' element={<Login />} />
-          <Route exact path='/home' element={<Home />} />
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/home" element={<Home />} />
         </Routes>
       </Router>
     </div>
